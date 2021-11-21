2b37c1eb9e055d5520cf415fda3bd85b7e5fc5f0
Jakub Filo <jakub@filo.dev>
2021-11-21 21:12:18 +0100

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
-		origins 'http://localhost:3010'
+		origins 'http://localhost:3010', 'https://www.skladis.com'
 
 		resource '*',
 		         headers: :any,
