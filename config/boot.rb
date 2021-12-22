f9e7c6adbb002720bc1cc0d317c7e959bf64906e
Jakub Filo <jakub@filo.dev>
2021-12-22 17:46:49 +0100

    remove deprecated autoload_paths_cache option

--- config/boot.rb
+++ config/boot.rb
@@ -8,7 +8,6 @@ Bootsnap.setup(
 		cache_dir:            'tmp/cache',          # Path to your cache
 		development_mode:     env == 'development', # Current working environment, e.g. RACK_ENV, RAILS_ENV, etc
 		load_path_cache:      true,                 # Optimize the LOAD_PATH with a cache
-		autoload_paths_cache: true,                 # Optimize ActiveSupport autoloads with cache
 		compile_cache_iseq:   true,                 # Compile Ruby code into ISeq cache, breaks coverage reporting.
 		compile_cache_yaml:   true                  # Compile YAML into a cache
 )
