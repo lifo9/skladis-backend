678ea694dfcec80c86cfb77fe1f343a3478eb17f
Jakub Filo <jakub@filo.dev>
2021-12-28 00:16:32 +0100

    expose headers for pagination

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -22,6 +22,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 		resource '*',
 		         headers: :any,
 		         credentials: true,
-		         methods: [:get, :post, :put, :patch, :delete, :options, :head]
+		         methods: [:get, :post, :put, :patch, :delete, :options, :head],
+						 expose: %w[Per-Page Total Link]
 	end
 end
