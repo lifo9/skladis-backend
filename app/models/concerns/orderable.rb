f1be6dd2b35479d1e962b446e08fc08275608c34
Jakub Filo <jakub@filo.dev>
2021-12-29 19:51:02 +0100

    orderable fix

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -3,7 +3,7 @@ module Orderable
 
   included do
     scope :api_order_by, -> (order_by, order) {
-      if (order_by && column_names.exclude?(order_by)) || (order && [:asc, :desc].exclude?(order.to_sym))
+      if (order_by.present? && column_names.exclude?(order_by)) || (order.present? && [:asc, :desc].exclude?(order.to_sym))
         return # prevent SQL Injection
       end
 
