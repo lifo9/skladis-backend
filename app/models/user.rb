79bb10900f870683aa8f3015e141ba017ae4a44b
Jakub Filo <jakub@filo.dev>
2021-12-30 22:33:39 +0100

    reorder user model params

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
   after_create :assign_default_role
 
   has_secure_password
+  has_one :registration_invitation, dependent: :destroy
 
   validates :email, :uniqueness => { :case_sensitive => false }
-
-  has_one :registration_invitation, dependent: :destroy
 
   def assign_default_role
     self.add_role(:user) if self.roles.blank?
