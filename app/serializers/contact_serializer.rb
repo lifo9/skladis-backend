6c7d1b0a007e73a9e9f6d914e88099d5a58945d9
Jakub Filo <jakub@filo.dev>
2022-01-30 21:38:07 +0100

    Update contact serializer

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,4 +1,8 @@
 class ContactSerializer < ApiSerializer
 
   attributes *Contact::PERMITTED_PARAMS
+
+  attribute :avatar do |user|
+    attachment_url(user.avatar.variant(:thumb)) if user.avatar.attached?
+  end
 end
