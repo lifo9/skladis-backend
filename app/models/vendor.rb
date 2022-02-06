1204758120249384b5de15bf74558f1d1ce69645
Jakub Filo <jakub@filo.dev>
2022-02-06 19:59:55 +0100

    after callbacks goes before! has_x attached

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,11 +2,11 @@ class Vendor < ApplicationRecord
   include Searchable
   include Orderable
 
+  after_save_commit :resize_logo
+
   has_one_attached :logo do |attachable|
     attachable.variant(:thumb, resize_to_limit: [256, 256])
   end
-
-  after_save_commit :resize_logo
 
   PERMITTED_PARAMS = [:name, :url, :logo].freeze
 
