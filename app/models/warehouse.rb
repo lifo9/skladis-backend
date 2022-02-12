6ae15a427a608098bd4dd3694ed036161c2e6d92
Jakub Filo <jakub@filo.dev>
2022-02-13 00:09:53 +0100

    Warehouse has many rooms

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,5 +2,7 @@ class Warehouse < ApplicationRecord
   include Searchable
   include Orderable
 
+  has_many :rooms
+
   PERMITTED_PARAMS = [:name, :street_name, :street_number, :city, :country, coordinates: []].freeze
 end
