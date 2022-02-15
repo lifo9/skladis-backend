276b456ad02a4afe6f48cfdc23b57aa79479cc45
Jakub Filo <jakub@filo.dev>
2022-02-15 17:48:44 +0100

    delete address on warehouse delete

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,4 @@
 class Address < ApplicationRecord
-
   PERMITTED_PARAMS = [:street_name, :street_number, :city, :zip, :country, coordinates: []].freeze
   PERMITTED_PARAMS_WITHOUT_COORDINATES = [:street_name, :street_number, :city, :zip, :country].freeze
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,7 +2,7 @@ class Warehouse < ApplicationRecord
   include Searchable
   include Orderable
 
-  belongs_to :address, class_name: Address.to_s
+  belongs_to :address, class_name: Address.to_s, dependent: :destroy
   has_many :rooms, class_name: Room.to_s
 
   PERMITTED_PARAMS = [:name, :address_id].freeze
