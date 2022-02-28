9b47559dbb74072cc311a2c00b249dc512074aab
Jakub Filo <jakub@filo.dev>
2022-02-28 15:06:04 +0100

    order product images by created_at

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,7 +6,7 @@ class ProductSerializer < ApiSerializer
   attribute :images do |product, params|
     images_url = []
 
-    product.images.each do |image|
+    product.images.sort_by(&:created_at).each do |image|
       type = params[:image_type] || :thumb
       images_url.push({
                         id: image.id,
