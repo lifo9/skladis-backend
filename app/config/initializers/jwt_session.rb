6ce19fb254af3774e858b9921d2d8a040f7d2be6
Jakub Filo <jakub@filo.dev>
2025-08-05 15:20:52 +0200

    Set JWT access_exp_time to 1 day

--- app/config/initializers/jwt_session.rb
+++ app/config/initializers/jwt_session.rb
@@ -1,2 +1,3 @@
-JWTSessions.algorithm = "HS256"
-JWTSessions.encryption_key = Rails.application.credentials[Rails.env.to_sym][:secret_jwt_encryption_key]
+JWTSessions.algorithm       = "HS256"
+JWTSessions.access_exp_time = 1.day
+JWTSessions.encryption_key  = Rails.application.credentials[Rails.env.to_sym][:secret_jwt_encryption_key]
