dbe50c476dc65e914d360039c901d729511aa0fb
Jakub Filo <jakub@filo.dev>
2025-08-05 19:08:09 +0200

    Fix cached variables

--- app/app/services/stock_audit_service.rb
+++ app/app/services/stock_audit_service.rb
@@ -1,13 +1,13 @@
-require 'caxlsx'
-
 class StockAuditService
   def self.build_current_stock_audit
+    unit_prices = latest_product_prices_hash
+
     Axlsx::Package.new do |p|
       p.workbook.add_worksheet(name: "Stock Audit") do |sheet|
         sheet.add_row ["Product Code", "Product Name", "Supplier", "Current Stock", "Unit Price", "Total Value"]
         Product.includes(:stocks, :suppliers).find_each(batch_size: 1000) do |product|
           in_stock       = product.stocks.sum(&:pieces)
-          unit_price     = get_latest_unit_price(product.id)
+          unit_price     = unit_prices[product.id] || 0
           supplier_names = get_supplier_names(product)
           total_value    = in_stock * unit_price
           sheet.add_row [
@@ -26,32 +26,25 @@ class StockAuditService
 
   private
 
-  def self.get_latest_unit_price(product_id)
-    @unit_prices ||= latest_product_prices_hash
-    @unit_prices[product_id] || 0
-  end
-
   def self.get_supplier_names(product)
     product.suppliers.pluck(:name).join(', ')
   end
 
   def self.latest_product_prices_hash
     sql = <<-SQL
-    SELECT product_id, unit_price
-    FROM (
-      SELECT product_id, unit_price,
-             ROW_NUMBER() OVER (PARTITION BY product_id 
-                               ORDER BY invoices.invoice_date DESC, 
-                                       invoice_items.id DESC) as rn
-      FROM invoice_items
-      JOIN invoices ON invoices.id = invoice_items.invoice_id
-    ) ranked_items
-    WHERE rn = 1
+      SELECT product_id, unit_price
+      FROM (
+        SELECT product_id, unit_price,
+               ROW_NUMBER() OVER (PARTITION BY product_id 
+                                 ORDER BY invoices.invoice_date DESC, 
+                                         invoice_items.id DESC) as rn
+        FROM invoice_items
+        JOIN invoices ON invoices.id = invoice_items.invoice_id
+      ) ranked_items
+      WHERE rn = 1
     SQL
 
-    @latest_product_prices ||= begin
-                                 results = ActiveRecord::Base.connection.execute(sql)
-                                 results.to_a.map { |row| [row['product_id'], row['unit_price']] }.to_h
-                               end
+    results = ActiveRecord::Base.connection.execute(sql)
+    results.to_a.map { |row| [row['product_id'], row['unit_price']] }.to_h
   end
 end
